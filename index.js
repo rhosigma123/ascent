e40a7aacc7b16b33a364050de58fa38e150e7e8e
Rhosigma <rhosigma.it@gmail.com>
2023-08-14 15:18:28 +0530

    image issue

--- index.js
+++ index.js
@@ -5,16 +5,7 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-var options = {
-    viewEngine: {
-    extname: '.hbs',                      
-    layoutsDir: 'views/layouts/',                      
-    defaultLayout : 'template.hbs',                      
-    partialsDir : 'views/partials/'                  
-   },
-   viewPath: 'views/emails/',
-   extName: '.hbs'
-};
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'assets')));
 
